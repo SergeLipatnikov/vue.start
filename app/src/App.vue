d1981feabd3a56c3432b407f8c04c0467503114b
SergeLipatnikov <qiu005b@gmail.com>
2021-11-15 07:40:04 +0300

    les_1

---
+++ app/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+     <div class="display">
+       <input v-model="operand1"/>
+       <input v-model="operand2"/>
+       = {{result}}
+     </div>
+     <div class="keyboard">
+   <button v-on:click="result = operand1 + operand2">+</button>
+   <button v-on:click="result = operand1 - operand2">-</button>
+   <button @click="result = operand1 / operand2">/</button>
+   <button @click="result = operand1 * operand2">*</button>
+ </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+   name: 'Calculator',
+   data(){
+       return {
+           operand1: 0,
+           operand2: 0,
+           result: 0
+       }
+   }
+ }
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
