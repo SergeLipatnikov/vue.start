7ee666a3a6b07fd80a86e42e9e13822d6fcc1ee9
SergeLipatnikov <qiu005b@gmail.com>
2021-11-15 07:42:42 +0300

    les_1.1

--- app/src/App.vue
+++ app/src/App.vue
@@ -31,12 +31,10 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.display {
+  display: flex;
+}
+.keyboard {
+  margin-top: 15px;
 }
 </style>
